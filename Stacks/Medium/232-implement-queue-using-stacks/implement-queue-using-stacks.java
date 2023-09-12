b2d8f2bbade3fc1b3457732e718226bbfd6bcb6c
Vandana <vandanakumari13998@gmail.com>
2023-09-13 00:27:07 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (93.93%) - LeetSync

--- Stacks/Medium/232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ Stacks/Medium/232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -2,6 +2,7 @@ class MyQueue {
 
     Stack<Integer> stack1;
     Stack<Integer> stack2;
+    int front;
 
     public MyQueue() {
         stack1 = new Stack<>();
@@ -10,29 +11,31 @@ class MyQueue {
     
     public void push(int x) {
         if(stack1.isEmpty()) {
-            stack1.push(x);
-        } else {
-            while(!stack1.isEmpty()) {
-                int popped = stack1.pop();
-                stack2.push(popped);
-            }
-            stack1.push(x);
-            while(!stack2.isEmpty()) {
-                stack1.push(stack2.pop());
-            }
+            front = x;
         }
+        stack1.push(x);
     }
     
     public int pop() {
-        return stack1.pop();
+        if(!stack2.isEmpty()) {
+            return stack2.pop();
+        } else {
+            while(!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+            return stack2.pop();
+        }
     }
     
     public int peek() {
-        return stack1.peek();
+        if(!stack2.isEmpty()) {
+            return stack2.peek();
+        }
+        return front;
     }
     
     public boolean empty() {
-        return stack1.isEmpty();
+        return stack1.isEmpty() && stack2.isEmpty();
     }
 }
 
