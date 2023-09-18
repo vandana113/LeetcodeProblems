3c6e4544e66dd27082d19ae8f32bbea866c02f0e
Vandana <vandanakumari13998@gmail.com>
2023-09-19 00:32:03 +0530

    Time: 873 ms (5.03%) | Memory: 43.9 MB (24.27%) - LeetSync

--- Trees/Medium/2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.java
+++ Trees/Medium/2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.java
@@ -3,30 +3,33 @@ class Solution {
     int n; 
     public int maximumDetonation(int[][] bombs) {
         n = bombs.length;
-        boolean [] visited;
         int maxBomb = 0;
         for(int i=0; i<n; i++) {
-            visited = new boolean[n];
             count = 0;
-            dfs(visited, i, bombs);
+            System.out.println("-----");
+            iterativeDfs(i, bombs);
             maxBomb = Math.max(count, maxBomb);
         }
         return maxBomb;
     }
 
-    public void dfs(boolean[] visited, int i, int [][] bombs) {
-
-        if(visited[i]) {
-            return;
-        }
-
-        count++;
-        visited[i] = true;
-        for(int j=0; j<n; j++) {
-            if(!visited[j] && connected(bombs[i][0], bombs[i][1], bombs[j][0],bombs[j][1],bombs[i][2])) {
-                dfs(visited, j, bombs);
+    public void iterativeDfs(int i, int [][] bombs) {
+        Stack<Integer> stack = new Stack<>();
+        Set <Integer> visited = new HashSet<>();
+        stack.push(i);
+        visited.add(i);
+        while(!stack.isEmpty()) {
+            int popped = stack.pop();
+            System.out.println("pod "+popped);
+            for(int j=0; j<n; j++) {
+                if(!visited.contains(j) && connected(bombs[popped][0], bombs[popped][1], bombs[j][0],bombs[j][1],bombs[popped][2])) {
+                    visited.add(j);
+                    // System.out.println("neigh "+j + visited[j]);
+                    stack.push(j);
+                }
             }
         }
+        count = visited.size();
     }
 
     public boolean connected(int x1, int y1, int x2, int y2, int r) {
