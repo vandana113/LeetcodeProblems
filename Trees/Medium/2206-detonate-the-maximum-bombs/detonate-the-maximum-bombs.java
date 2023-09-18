c23a31bb305f5e83151abbf63b94a2f446aea650
Vandana <vandanakumari13998@gmail.com>
2023-09-19 00:34:22 +0530

    Time: 155 ms (41.11%) | Memory: 44.1 MB (14.09%) - LeetSync

--- Trees/Medium/2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.java
+++ Trees/Medium/2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.java
@@ -6,12 +6,9 @@ class Solution {
         // Build the graph
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == j) {
-                    continue;
-                }
                 int xi = bombs[i][0], yi = bombs[i][1], ri = bombs[i][2];
                 int xj = bombs[j][0], yj = bombs[j][1];
-
+                
                 // Create a path from node i to node j, if bomb i detonates bomb j.
                 if ((long)ri * ri >= (long)(xi - xj) * (xi - xj) + (long)(yi - yj) * (yi - yj)) {
                     graph.computeIfAbsent(i, k -> new ArrayList<>()).add(j);
@@ -21,23 +18,23 @@ class Solution {
         
         int answer = 0;
         for (int i = 0; i < n; i++) {
-            answer = Math.max(answer, dfs(i, graph));
+            answer = Math.max(answer, bfs(i, graph));
         }
         
         return answer;
     }
     
-    private int dfs(int i, Map<Integer, List<Integer>> graph) {
-        Stack<Integer> stack = new Stack<>();
+    private int bfs(int i, Map<Integer, List<Integer>> graph) {
+        Deque<Integer> queue = new ArrayDeque<>();
         Set<Integer> visited = new HashSet<>();
-        stack.push(i);
+        queue.offer(i);
         visited.add(i);
-        while (!stack.isEmpty()) {
-            int cur = stack.pop();
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
             for (int neib : graph.getOrDefault(cur, new ArrayList<>())) {
                 if (!visited.contains(neib)) {
                     visited.add(neib);
-                    stack.push(neib);
+                    queue.offer(neib);
                 }
             }
         }
