e32398cf0e3e5f1ee258d9467ea612b47cf6fdba
Vandana <vandanakumari13998@gmail.com>
2023-09-19 00:33:42 +0530

    Time: 161 ms (32.22%) | Memory: 44.2 MB (14.09%) - LeetSync

--- Trees/Medium/2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.java
+++ Trees/Medium/2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.java
@@ -1,36 +1,42 @@
 class Solution {
-    int count = 0;
-    int n; 
     public int maximumDetonation(int[][] bombs) {
-        n = bombs.length;
-        boolean [] visited;
-        int maxBomb = 0;
-        for(int i=0; i<n; i++) {
-            visited = new boolean[n];
-            count = 0;
-            dfs(visited, i, bombs);
-            maxBomb = Math.max(count, maxBomb);
-        }
-        return maxBomb;
-    }
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        int n = bombs.length;
 
-    public void dfs(boolean[] visited, int i, int [][] bombs) {
+        // Build the graph
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    continue;
+                }
+                int xi = bombs[i][0], yi = bombs[i][1], ri = bombs[i][2];
+                int xj = bombs[j][0], yj = bombs[j][1];
 
-        if(visited[i]) {
-            return;
-        }
-
-        count++;
-        visited[i] = true;
-        for(int j=0; j<n; j++) {
-            if(!visited[j] && connected(bombs[i][0], bombs[i][1], bombs[j][0],bombs[j][1],bombs[i][2])) {
-                dfs(visited, j, bombs);
+                // Create a path from node i to node j, if bomb i detonates bomb j.
+                if ((long)ri * ri >= (long)(xi - xj) * (xi - xj) + (long)(yi - yj) * (yi - yj)) {
+                    graph.computeIfAbsent(i, k -> new ArrayList<>()).add(j);
+                }
             }
         }
+
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            int count = dfs(i, new HashSet<>(), graph);
+            answer = Math.max(answer, count);
+        }
+
+        return answer;
     }
 
-    public boolean connected(int x1, int y1, int x2, int y2, int r) {
-        long distance = (long)Math.pow(x2-x1, 2) + (long)Math.pow(y2-y1, 2);
-        return distance <= ((long)Math.pow(r,2));
+    // DFS to get the number of nodes reachable from a given node cur
+    private int dfs(int cur, Set<Integer> visited, Map<Integer, List<Integer>> graph) {
+        visited.add(cur);
+        int count = 1;
+        for (int neib : graph.getOrDefault(cur, new ArrayList<>())) {
+            if (!visited.contains(neib)) {
+                count += dfs(neib, visited, graph);
+            }
+        }
+        return count;
     }
 }
