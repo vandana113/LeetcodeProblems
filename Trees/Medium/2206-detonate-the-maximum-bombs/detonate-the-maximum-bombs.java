bac5d2311f03c55de31121845a75b330e3940af2
Vandana <vandanakumari13998@gmail.com>
2023-09-19 00:34:34 +0530

    Time: 147 ms (49.71%) | Memory: 42.8 MB (97.78%) - LeetSync

--- Trees/Medium/2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.java
+++ Trees/Medium/2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.java
@@ -1,43 +1,36 @@
 class Solution {
+    int count = 0;
+    int n; 
     public int maximumDetonation(int[][] bombs) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        int n = bombs.length;
-        
-        // Build the graph
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int xi = bombs[i][0], yi = bombs[i][1], ri = bombs[i][2];
-                int xj = bombs[j][0], yj = bombs[j][1];
-                
-                // Create a path from node i to node j, if bomb i detonates bomb j.
-                if ((long)ri * ri >= (long)(xi - xj) * (xi - xj) + (long)(yi - yj) * (yi - yj)) {
-                    graph.computeIfAbsent(i, k -> new ArrayList<>()).add(j);
-                }
-            }
+        n = bombs.length;
+        boolean [] visited;
+        int maxBomb = 0;
+        for(int i=0; i<n; i++) {
+            visited = new boolean[n];
+            count = 0;
+            dfs(visited, i, bombs);
+            maxBomb = Math.max(count, maxBomb);
         }
-        
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            answer = Math.max(answer, bfs(i, graph));
-        }
-        
-        return answer;
+        return maxBomb;
     }
-    
-    private int bfs(int i, Map<Integer, List<Integer>> graph) {
-        Deque<Integer> queue = new ArrayDeque<>();
-        Set<Integer> visited = new HashSet<>();
-        queue.offer(i);
-        visited.add(i);
-        while (!queue.isEmpty()) {
-            int cur = queue.poll();
-            for (int neib : graph.getOrDefault(cur, new ArrayList<>())) {
-                if (!visited.contains(neib)) {
-                    visited.add(neib);
-                    queue.offer(neib);
-                }
+
+    public void dfs(boolean[] visited, int i, int [][] bombs) {
+
+        if(visited[i]) {
+            return;
+        }
+
+        count++;
+        visited[i] = true;
+        for(int j=0; j<n; j++) {
+            if(!visited[j] && connected(bombs[i][0], bombs[i][1], bombs[j][0],bombs[j][1],bombs[i][2])) {
+                dfs(visited, j, bombs);
             }
         }
-        return visited.size();
+    }
+
+    public boolean connected(int x1, int y1, int x2, int y2, int r) {
+        long distance = (long)Math.pow(x2-x1, 2) + (long)Math.pow(y2-y1, 2);
+        return distance <= ((long)Math.pow(r,2));
     }
 }
