2bcc499043bcccf235b42a4c0c2e98dc20451702
Vandana <vandanakumari13998@gmail.com>
2023-10-04 22:10:38 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (31.60%) - LeetSync

--- Arrays/Easy/48-rotate-image/rotate-image.java
+++ Arrays/Easy/48-rotate-image/rotate-image.java
@@ -10,13 +10,6 @@ class Solution {
         }
 
         for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                System.out.print(matrix[i][j]+" ");
-            }
-            System.out.println();
-        }
-
-        for(int i=0; i<n; i++) {
             for(int j=0; j<=(n-1)/2; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[i][n-j-1];
