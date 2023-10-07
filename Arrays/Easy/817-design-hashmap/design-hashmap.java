999a6dbdab99a913cdd5c875fba1a52339540055
Vandana <vandanakumari13998@gmail.com>
2023-10-07 18:16:18 +0530

    Time: 20 ms (61.36%) | Memory: 50.6 MB (55.48%) - LeetSync

--- Arrays/Easy/817-design-hashmap/design-hashmap.java
+++ Arrays/Easy/817-design-hashmap/design-hashmap.java
@@ -1,87 +1,81 @@
-class Pair<U, V> {
-  public U first;
-  public V second;
+class Pair<S, T> {
+    public S first;
+    public T second;
 
-  public Pair(U first, V second) {
-    this.first = first;
-    this.second = second;
-  }
+    public Pair(S first, T second) {
+        this.first = first;
+        this.second = second;
+    }
 }
-
 
 class Bucket {
-  private List<Pair<Integer, Integer>> bucket;
+    List<Pair<Integer, Integer>> bucket;
 
-  public Bucket() {
-    this.bucket = new LinkedList<Pair<Integer, Integer>>();
-  }
-
-  public Integer get(Integer key) {
-    for (Pair<Integer, Integer> pair : this.bucket) {
-      if (pair.first.equals(key))
-        return pair.second;
+    Bucket(){
+        this.bucket = new ArrayList<>();
     }
-    return -1;
-  }
 
-  public void update(Integer key, Integer value) {
-    boolean found = false;
-    for (Pair<Integer, Integer> pair : this.bucket) {
-      if (pair.first.equals(key)) {
-        pair.second = value;
-        found = true;
-      }
+    public void update(Integer key, Integer value) {
+        boolean found = false;
+        for(Pair<Integer,Integer> pair: this.bucket) {
+            if(pair.first.equals(key)) {
+                pair.second = value;
+                found = true;
+            }
+        }
+        if(!found) {
+            this.bucket.add(new Pair<Integer,Integer>(key, value));
+        }
     }
-    if (!found)
-      this.bucket.add(new Pair<Integer, Integer>(key, value));
-  }
 
-  public void remove(Integer key) {
-    for (Pair<Integer, Integer> pair : this.bucket) {
-      if (pair.first.equals(key)) {
-        this.bucket.remove(pair);
-        break;
-      }
+    public Integer get(Integer key) {
+        for(Pair<Integer,Integer> pair: this.bucket) {
+            if(pair.first.equals(key)) {
+                return pair.second;
+            }
+        }
+        return -1;
     }
-  }
+
+    public void remove(Integer key) {
+        for(Pair<Integer,Integer> pair: this.bucket) {
+            if(pair.first.equals(key)) {
+                this.bucket.remove(pair);
+                break;
+            }
+        }
+    }
 }
-
 class MyHashMap {
-  private int key_space;
-  private List<Bucket> hash_table;
-
-  /** Initialize your data structure here. */
-  public MyHashMap() {
-    this.key_space = 2069;
-    this.hash_table = new ArrayList<Bucket>();
-    for (int i = 0; i < this.key_space; ++i) {
-      this.hash_table.add(new Bucket());
+    int n = 2069;
+    List<Bucket> map;
+    public MyHashMap() {
+        map = new ArrayList<>();
+        for(int i=0; i<2069; i++) {
+            this.map.add(new Bucket());
+        }
     }
-  }
-
-  /** value will always be non-negative. */
-  public void put(int key, int value) {
-    int hash_key = key % this.key_space;
-    this.hash_table.get(hash_key).update(key, value);
-  }
-
-  /**
-   * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping
-   * for the key
-   */
-  public int get(int key) {
-    int hash_key = key % this.key_space;
-    return this.hash_table.get(hash_key).get(key);
-  }
-
-  /** Removes the mapping of the specified value key if this map contains a mapping for the key */
-  public void remove(int key) {
-    int hash_key = key % this.key_space;
-    this.hash_table.get(hash_key).remove(key);
-  }
+    
+    public void put(int key, int value) {
+        int hashValue = key%n;
+        this.map.get(hashValue).update(key, value);
+    }
+    
+    public int get(int key) {
+        int hashValue = key%n;
+        return this.map.get(hashValue).get(key);
+    }
+    
+    public void remove(int key) {
+        int hashValue = key%n;
+        this.map.get(hashValue).remove(key);
+    }
 }
 
 /**
- * Your MyHashMap object will be instantiated and called as such: MyHashMap obj = new MyHashMap();
- * obj.put(key,value); int param_2 = obj.get(key); obj.remove(key);
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
  */
