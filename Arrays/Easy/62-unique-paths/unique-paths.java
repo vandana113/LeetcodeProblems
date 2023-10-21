e64c56630729e12fda63127ce8cd89768370c3cd
Vandana <vandanakumari13998@gmail.com>
2023-10-21 17:09:50 +0530

    Time: 0 ms (100.00%) | Memory: 39.3 MB (47.07%) - LeetSync

--- Arrays/Easy/62-unique-paths/unique-paths.java
+++ Arrays/Easy/62-unique-paths/unique-paths.java
@@ -4,15 +4,15 @@ class Solution {
     }
 
     public int uniquePathsHelper(int m, int n) {
-        int dp[][] = new int[m+1][n+1];
+        int dp[][] = new int[m][n];
 
-        dp[m][n] = 1;
+        dp[m-1][n-1] = 1;
 
 
 
-        for(int i = m; i>=1; i--) {
-            for(int j = n; j>=1; j--) {
-                if(i==m || j==n) {
+        for(int i = m-1; i>=0; i--) {
+            for(int j = n-1; j>=0; j--) {
+                if(i==m-1 || j==n-1) {
                     dp[i][j] = 1;
                 } else {
                     dp[i][j] = dp[i+1][j] + dp[i][j+1];
@@ -20,6 +20,6 @@ class Solution {
             }
         }
 
-        return dp[1][1];
+        return dp[0][0];
     }
 }
