4edda1e02190d7a4db02a37662a3c0fab726230d
Vandana <vandanakumari13998@gmail.com>
2023-10-25 01:52:18 +0530

    Time: 1035 ms (9.27%) | Memory: 62.7 MB (8.52%) - LeetSync

--- Arrays/Easy/128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ Arrays/Easy/128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -10,15 +10,13 @@ class Solution {
             if(!map.contains(num-1)) {
                 int temp = num;
                 int len = 1;
-                while(map.contains(++temp)) {
+                while(map.contains(temp+1)) {
+                    temp++;
                     len++;
                 }
                 maxLen = Math.max(maxLen, len);
             }
         }
         return maxLen;
-
-        // 4:1, 1:1, 3:1, 2:1 -> 4:2, 1:1, 3:2, 2:2
-        // 3:4, 1:0, 2:2, 4:2
     }
 }
