6b3d04fdd5921e91c8dff9668ffc9a47ab7afeb5
Vandana <vandanakumari13998@gmail.com>
2023-10-25 01:50:45 +0530

    Time: 25 ms (71.58%) | Memory: 57 MB (54.41%) - LeetSync

--- Arrays/Easy/128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ Arrays/Easy/128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,24 +1,26 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        HashSet<Integer> map = new HashSet<>();
-        int maxLen = 0;
-        for(int num: nums) {
-            map.add(num);
+        Set<Integer> num_set = new HashSet<Integer>();
+        for (int num : nums) {
+            num_set.add(num);
         }
 
-        for(int num: nums) {
-            if(!map.contains(num-1)) {
-                int temp = num;
-                int len = 1;
-                while(map.contains(++temp)) {
-                    len++;
+        int longestStreak = 0;
+
+        for (int num : num_set) {
+            if (!num_set.contains(num-1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (num_set.contains(currentNum+1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
                 }
-                maxLen = Math.max(maxLen, len);
+
+                longestStreak = Math.max(longestStreak, currentStreak);
             }
         }
-        return maxLen;
 
-        // 4:1, 1:1, 3:1, 2:1 -> 4:2, 1:1, 3:2, 2:2
-        // 3:4, 1:0, 2:2, 4:2
+        return longestStreak;
     }
 }
