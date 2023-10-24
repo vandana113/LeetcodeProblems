e4de4dc93288382e48202a359e18bee42426461f
Vandana <vandanakumari13998@gmail.com>
2023-10-24 18:36:31 +0530

    Time: 224 ms (10.36%) | Memory: 44.7 MB (8.14%) - LeetSync

--- Arrays/Easy/18-4sum/4sum.java
+++ Arrays/Easy/18-4sum/4sum.java
@@ -1,5 +1,6 @@
 class Solution {
     List<List<Integer>> result = new ArrayList<>();
+
     public List<List<Integer>> fourSum(int[] nums, int target) {
         int n = nums.length;
         if(n == 0) {
@@ -7,49 +8,44 @@ class Solution {
         }
         
         Arrays.sort(nums);
-        for(int i=0; i<n; i++) {
-            if(i!=0 && nums[i] == nums[i-1]) {
+
+        return kSum(nums, target, 0, 4);
+    }
+    public List<List<Integer>> kSum(int[] nums, long target, int start, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+
+        if (k == 2) {
+            return twoSum(nums, target, start);
+        }
+
+        for(int i=start; i<nums.length; i++) {
+            if(i!=start && nums[i] == nums[i-1]) {
                 continue;
             }
-            for(int j=i+1; j<n; j++) {
-                if(j!=i+1 && nums[j] == nums[j-1]) {
-                    continue;
-                }
-                
-                List<List<Integer>>  subset = twoSum(nums, j+1, n-1, (long)target - (long)nums[i] - (long)nums[j], nums[i], nums[j]);
 
-                for(List<Integer> set: subset) {
-                    List<Integer> resultSubset = new ArrayList<>();
-                    resultSubset.add(nums[i]);
-                    resultSubset.add(nums[j]);
+            for(List<Integer> subset: kSum(nums, target - nums[i], i+1, k-1)) {
+                result.add(new ArrayList<>(Arrays.asList(nums[i])));
 
-                    resultSubset.add(set.get(0));
-                    resultSubset.add(set.get(1));
-
-                    result.add(resultSubset);
-                }
+                result.get(result.size() - 1).addAll(subset);
             }
         }
         return result;
     }
 
-    public List<List<Integer>> twoSum(int [] nums, int start, int end, long targetSum, int num1, int num2) {
+    public List<List<Integer>> twoSum(int [] nums, long targetSum, int start) {
         
 
-        HashMap<Long, Integer> map= new HashMap<>();
+        HashSet<Long> map= new HashSet<>();
         List<List<Integer>> res = new ArrayList<>();    
 
-        if(end-start+1<2){
-            return res; 
-        }
-
-        for(int i=start; i<=end; i++) {
+        for(int i=start; i<nums.length; i++) {
             if(res.isEmpty() || res.get(res.size() - 1).get(1) != nums[i]){  
-                if(map.containsKey(targetSum-nums[i])) {
+                if(map.contains(targetSum - nums[i])) {
                     res.add(Arrays.asList((int)targetSum - nums[i], nums[i]));
                 }
             }
-            map.put((long)nums[i], i);
+            map.add((long)nums[i]);
         }
         return res;
     }
