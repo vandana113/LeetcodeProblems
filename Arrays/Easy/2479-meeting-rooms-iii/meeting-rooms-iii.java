ef9fd496463e8dac2983bfb52bf4b0941a9d6465
Vandana <vandanakumari13998@gmail.com>
2023-10-31 16:20:57 +0530

    Time: 76 ms (40.15%) | Memory: 100.3 MB (19.07%) - LeetSync

--- Arrays/Easy/2479-meeting-rooms-iii/meeting-rooms-iii.java
+++ Arrays/Easy/2479-meeting-rooms-iii/meeting-rooms-iii.java
@@ -8,15 +8,9 @@ class Solution {
         });
 
         PriorityQueue<Integer> freeRooms = new PriorityQueue<>();
-
-        PriorityQueue<Pair<int[],Integer>> occupiedRooms = new PriorityQueue<>(new Comparator<>() {
-            public int compare(Pair<int[],Integer> meeting1, Pair<int[],Integer> meeting2) {
-                if(meeting1.getKey()[1] == meeting2.getKey()[1]){
-                    return meeting1.getValue() - meeting2.getValue();
-                }
-                return meeting1.getKey()[1] - meeting2.getKey()[1];
-            }
-        });
+        
+        // end Time and room number
+        PriorityQueue<int []> occupiedRooms = new PriorityQueue<>(new OccupiedComparator());
 
         for(int i=0; i<n; i++) {
             freeRooms.add(i);
@@ -24,25 +18,22 @@ class Solution {
 
 
         for(int i=0; i<meetings.length; i++) {
-            while(!occupiedRooms.isEmpty() && occupiedRooms.peek().getKey()[1] <= meetings[i][0]) {
-                int roomNumber = occupiedRooms.peek().getValue();
+            while(!occupiedRooms.isEmpty() && occupiedRooms.peek()[0] <= meetings[i][0]) {
+                int roomNumber = occupiedRooms.peek()[1];
                 occupiedRooms.remove();
                 freeRooms.add(roomNumber);
             }
-            if(!freeRooms.isEmpty()) {
-                int roomNumber = freeRooms.remove();
-                rooms[roomNumber]++;
-                occupiedRooms.add(new Pair<>(new int[] {0, meetings[i][1]}, roomNumber));
-            } else {
+            int endTime = meetings[i][1];
+            if(freeRooms.isEmpty()) {
                 // No room empty, delay the current meeting
-                Pair<int[],Integer> endedMeeting = occupiedRooms.remove();
-                int endTime = endedMeeting.getKey()[1] + (meetings[i][1] - meetings[i][0]); 
-                int roomNumber = endedMeeting.getValue();
-                
-                int [] newMeetingTime = new int[]{0, endTime};
-                occupiedRooms.add(new Pair<>(newMeetingTime, roomNumber));
-                rooms[roomNumber]++;
+                endTime = occupiedRooms.peek()[0] + (meetings[i][1] - meetings[i][0]);
+                int roomNumber = occupiedRooms.peek()[1];
+                occupiedRooms.remove();
+                freeRooms.add(roomNumber);
             }
+            int roomNumber = freeRooms.remove();
+            occupiedRooms.add(new int[] {endTime, roomNumber});
+            rooms[roomNumber]++;
         }
 
         int maxMeetings = 0;
@@ -55,5 +46,14 @@ class Solution {
         }
 
         return roomNumber;
+    }
+
+    public class OccupiedComparator implements Comparator<int []> {
+        public int compare(int [] meeting1, int [] meeting2) {
+            if(meeting1[0] == meeting2[0]) {
+                return meeting1[1] - meeting2[1];
+            }
+            return meeting1[0] - meeting2[0];
+        }
     }
 }
