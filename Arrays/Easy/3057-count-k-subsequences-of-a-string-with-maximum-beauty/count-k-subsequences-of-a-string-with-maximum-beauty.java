073f5d921817ce59c671b4e8b3305f7e452bb5b4
Vandana <vandanakumari13998@gmail.com>
2023-11-05 00:05:43 +0530

    Time: 7 ms (44.47%) | Memory: 45 MB (47.56%) - LeetSync

--- Arrays/Easy/3057-count-k-subsequences-of-a-string-with-maximum-beauty/count-k-subsequences-of-a-string-with-maximum-beauty.java
+++ Arrays/Easy/3057-count-k-subsequences-of-a-string-with-maximum-beauty/count-k-subsequences-of-a-string-with-maximum-beauty.java
@@ -1,82 +1,90 @@
-public class Solution {
-    // Helper function to calculate binomial coefficient "n choose r" using logarithmic operations
-    private long nCr(long n, long r) {
-        if (r > n) 
-            return 0;
-    
-        if (r == 0 || n == r) 
-            return 1;
-    
-        double res = 0;
-        for (int i = 0; i < r; i++) {
-            res += (Math.log(n - i) - Math.log(i + 1));
-        }
-        
-        return Math.round(Math.exp(res));
-    }
-    
+class Solution {
+    int M = 1000000007;
     public int countKSubsequencesWithMaxBeauty(String s, int k) {
+        
+        int [] chars = new int[26];
         int n = s.length();
-        final long mod = 1000000007; // 10^9 + 7
-        
-        // Initialize an array to store the frequency of each character in the input string
-        int[] occ = new int[26];
-        
-        // Count the frequency of each character in the string
-        for (char ch : s.toCharArray()) {
-            occ[ch - 'a']++;
-        }
-        
-        // Initialize a priority queue to maintain the 'k' characters with the highest frequency
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        
-        // Iterate through the characters and update minHeap with the top 'k' characters
-        for (int i = 0; i < 26; i++) {
-            if (occ[i] != 0) {
-                if (minHeap.size() < k) {
-                    minHeap.offer(occ[i]);
-                } else if (occ[i] > minHeap.peek()) {
-                    minHeap.poll();
-                    minHeap.offer(occ[i]);
-                }
+        int [] freqChar = new int[n+1];
+        int unique = 0;
+        for(int i=0; i<n; i++) {
+            char cc = s.charAt(i);
+            chars[cc - 'a']++;
+        }       
+
+        // Sorting the chars
+        for(int i=0; i<chars.length; i++) {
+            if(chars[i]!=0) {
+                int freq = chars[i];
+                freqChar[freq]++;
+                unique++;
             }
         }
-        
-        // If the size of minHeap is less than 'k', return 0
-        if (minHeap.size() < k) {
+
+        if(k > unique) {
             return 0;
         }
         
-        // Initialize 'res' to 1, which will be used to calculate the result
-        long res = 1;
-        
-        // Create a map to store the frequencies of characters selected for the k-subsequence
-        Map<Integer, Integer> occFreqSubs = new HashMap<>();
-        
-        // Iterate through minHeap and update 'res' and occFreqSubs
-        while (!minHeap.isEmpty()) {
-            int it = minHeap.poll();
+        int i=n;
+        long result=1;
+        while(k>0 && i>=0) {
+            if(freqChar[i] == 0) {
+                i--;
+            } else if(k>=freqChar[i]) {
+                k -= freqChar[i]; 
+                result = (result * pow(i, freqChar[i])) % M;
+                i--;
+            } else {
+                break;
+            }
+        }
+
+        // for(int j=0; j<26; j++){
+        //     System.out.print(chars[j] +" ");
+        // }
+
+        // NcR
+        if(k>0) {
             
-            res = (res * it) % mod;
-            occFreqSubs.put(it, occFreqSubs.getOrDefault(it, 0) + 1);
+            long x = nCr(freqChar[i], k);
+            long power = pow(i, k);
+
+            long xPower = (x * power) % M;
+            // System.out.println("i " + i +" k "+k +" x "+x+" result "+result +" xPower "+xPower);
+            result = ((long)result * (long)xPower) % M;
         }
-        
-        // Create a map to store the total frequencies of characters in the input string
-        Map<Integer, Integer> occFreqTotal = new HashMap<>();
-        
-        // Populate occFreqTotal
-        for (int i = 0; i < 26; i++) {
-            occFreqTotal.put(occ[i], occFreqTotal.getOrDefault(occ[i], 0) + 1);
-        }
-        
-        // Iterate through occFreqSubs and update 'res' by considering combinations
-        for (Map.Entry<Integer, Integer> entry : occFreqSubs.entrySet()) {
-            int a = entry.getKey();
-            int b = entry.getValue();
-            res = (res * nCr(occFreqTotal.get(a), b)) % mod;
-        }
-        
-        // Return the final result modulo 10^9 + 7
-        return (int) (res % mod);
+    
+        return (int)result%M;
     }
+
+    private long nCr(int n, int k) {
+        long fact[] = new long[27];
+        long invFact[] = new long[27];
+
+        fact[1] = 1;
+
+        for(int i=2; i<=26; i++) {
+            fact[i] = (fact[i-1] * i) % M;
+        }
+        invFact[26] = pow(fact[26], M-2);
+
+        for(int i=25; i>=0; i--) {
+            invFact[i] = (invFact[i+1] * (i+1)) % M;
+        }
+        return (fact[n] * ((invFact[k] * invFact[n-k]) %M)) % M;
+    }
+
+    private long pow(long x, long n) {
+        long result = 1;
+        while(n != 0) {
+            if(n%2 != 0) {
+                result = (result * x) % M;
+            }
+
+            n=n/2;
+            x = (x*x) % M;
+        }
+
+        return result;
+    }
+    
 }
