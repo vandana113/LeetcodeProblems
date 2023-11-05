97bf32c117ca23b74163a296906882a037091654
Vandana <vandanakumari13998@gmail.com>
2023-11-05 23:15:06 +0530

    Time: 376 ms (5.29%) | Memory: 81.2 MB (49.59%) - LeetSync

--- Arrays/Easy/1953-finding-mk-average/finding-mk-average.java
+++ Arrays/Easy/1953-finding-mk-average/finding-mk-average.java
@@ -1,119 +1,123 @@
 class MKAverage {
-    PriorityQueue<Integer> left = new PriorityQueue<>((a, b) -> b - a);
-    PriorityQueue<Integer> right = new PriorityQueue<>((a, b) -> a - b);
-    PriorityQueue<Integer> minMiddle = new PriorityQueue<>((a, b) -> a - b);
-    PriorityQueue<Integer> maxMiddle = new PriorityQueue<>((a, b) -> b - a);
-    Queue<Integer> vals = new LinkedList<>();
-    Map<Integer, Integer> deleteLeft = new HashMap<>(), deleteRight = new HashMap<>(), deleteMinMiddle = new HashMap<>(), deleteMaxMiddle = new HashMap<>();
-    
-    long sum;
-    int m, k;
-    boolean flag;
+
+    TreeMap<Integer, Integer> l1 =new TreeMap<>();
+    TreeMap<Integer, Integer> l2 =new TreeMap<>();
+    TreeMap<Integer, Integer> l3 =new TreeMap<>();
+    Queue<Integer> queue = new LinkedList<>();
+    int l1Size = 0, l2Size = 0, l3Size = 0;
+    int m, k, n;
+    long totalSum = 0;
+
     public MKAverage(int m, int k) {
         this.m = m;
         this.k = k;
-        sum = 0;
-        flag = false;
+        this.n = 0;
     }
     
     public void addElement(int num) {
-        vals.offer(num);
-        
-        if (!flag && vals.size() == m) {
-            initialize();
-            flag = true;
-        }
-        else if (flag) {
-            int lv = vals.poll();
-            update(left, deleteLeft);
-            update(right, deleteRight);
-            if (!left.isEmpty() && left.peek() >= lv) {
-                update(minMiddle, deleteMinMiddle);
-                int v = minMiddle.poll();
-                deleteMaxMiddle.put(v, deleteMaxMiddle.getOrDefault(v, 0) + 1);
-                left.offer(v);
-                deleteLeft.put(lv, deleteLeft.getOrDefault(lv, 0) + 1);
-                sum -= v;
+        if(queue.size() == m) {
+            int key = queue.remove();
+            if(l1.containsKey(key)) {
+                l1.put(key, l1.get(key) - 1);
+                if(l1.get(key) == 0) {
+                    l1.remove(key);
+                }
+                l1Size--;
+            } else if(l2.containsKey(key)) {
+                l2.put(key, l2.get(key) - 1);
+                if(l2.get(key) == 0) {
+                    l2.remove(key);
+                }
+                l2Size--;
+                totalSum-=key;
+            } else if(l3.containsKey(key)) {
+                l3.put(key, l3.get(key) - 1);
+                if(l3.get(key) == 0) {
+                    l3.remove(key);
+                }
+                l3Size--;
             }
-            else if (!right.isEmpty() && right.peek() <= lv) {
-                update(maxMiddle, deleteMaxMiddle);
-                int v = maxMiddle.poll();
-                deleteMinMiddle.put(v, deleteMinMiddle.getOrDefault(v, 0) + 1);
-                right.offer(v);
-                deleteRight.put(lv, deleteRight.getOrDefault(lv, 0) + 1);
-                sum -= v;
+
+            if(l1Size < k) {
+                int midKey = l2.firstKey();
+                l1.put(midKey, l1.getOrDefault(midKey, 0) + 1);
+                l1Size++;
+
+                l2.put(midKey, l2.get(midKey) - 1);
+                if(l2.get(midKey) == 0) {
+                    l2.remove(midKey);
+                }
+                l2Size--;
+                totalSum-=midKey;
             }
-            else {
-                deleteMinMiddle.put(lv, deleteMinMiddle.getOrDefault(lv, 0) + 1);
-                deleteMaxMiddle.put(lv, deleteMaxMiddle.getOrDefault(lv, 0) + 1);
-                sum -= lv;
-            }
-            
-            update(left, deleteLeft);
-            update(right, deleteRight);
-            if (!left.isEmpty() && left.peek() >= num) {
-                int v = left.poll();
-                minMiddle.offer(v);
-                maxMiddle.offer(v);
-                left.offer(num);
-                sum += v;
-            }
-            else if (!right.isEmpty() && right.peek() <= num) {
-                int v = right.poll();
-                minMiddle.offer(v);
-                maxMiddle.offer(v);
-                right.offer(num);
-                sum += v;
-            }
-            else {
-                minMiddle.offer(num);
-                maxMiddle.offer(num);
-                sum += num;
+            if(l2Size < m- 2*k) {
+                int rightKey = l3.firstKey();
+                l2.put(rightKey, l2.getOrDefault(rightKey, 0) + 1);
+                l2Size++;
+                totalSum+=rightKey;
+
+                l3.put(rightKey, l3.get(rightKey) - 1);
+                if(l3.get(rightKey) == 0) {
+                    l3.remove(rightKey);
+                }
+                l3Size--;
             }
         }
+
+        queue.add(num);
+
+        if(l1Size < k || num <= l1.lastKey()) {
+            l1.put(num, l1.getOrDefault(num,0) + 1);
+            l1Size++;
+        } else if(l2Size < m-2*k || num <= l2.lastKey()) {
+            l2.put(num, l2.getOrDefault(num,0) + 1);
+            l2Size++;
+            totalSum+=num;
+        } else {
+            l3.put(num, l3.getOrDefault(num,0) + 1);
+            l3Size++;
+        }
+
+        if(l1Size>k) {
+            int leftKey = l1.lastKey();
+            l2.put(leftKey, l2.getOrDefault(leftKey, 0) + 1);
+            l2Size++;
+            totalSum+=leftKey;
+
+            l1.put(leftKey, l1.get(leftKey) - 1);
+            if(l1.get(leftKey) == 0) {
+                l1.remove(leftKey);
+            }
+            l1Size--;
+        } 
+        if(l2Size > m-2*k) {
+            int midKey = l2.lastKey();
+            l3.put(midKey, l3.getOrDefault(midKey, 0) + 1);
+            l3Size++;
+
+            l2.put(midKey, l2.get(midKey) - 1);
+            if(l2.get(midKey) == 0) {
+                l2.remove(midKey);
+            }
+            l2Size--;
+            totalSum-=midKey;
+        }
+
+    
+        System.out.println(num + " l1 " + l1Size + " l2 " + l2Size + " l3 "+ l3Size + " sum "+ totalSum);
     }
     
     public int calculateMKAverage() {
-        // System.out.println(sum);
-        return flag? (int)(sum / (m - 2 * k)) : -1;
-    }
-    
-    private void initialize() {
-        int i;
-        for (i = 0; i < m; ++i) {
-            int v = vals.poll();
-            minMiddle.offer(v);
-            maxMiddle.offer(v);
-            sum += v;
-            vals.offer(v);
+        if(queue.size() < m) {
+            return -1;
         }
-        i = 0;
-        while (i < k) {
-            update(minMiddle, deleteMinMiddle);
-            int v = minMiddle.poll();
-            left.offer(v);
-            deleteMaxMiddle.put(v, deleteMaxMiddle.getOrDefault(v, 0) + 1);
-            sum -= v;
-            i ++;
-        }
-        
-        i = 0;
-        while (i < k) {
-            update(maxMiddle, deleteMaxMiddle);
-            int v = maxMiddle.poll();
-            right.offer(v);
-            deleteMinMiddle.put(v, deleteMinMiddle.getOrDefault(v, 0) + 1);
-            sum -= v;
-            i ++;
-        }
-    }
-    
-    private void update(PriorityQueue<Integer> pq, Map<Integer, Integer> delete) {
-        while (!pq.isEmpty() && delete.containsKey(pq.peek()) && delete.get(pq.peek()) > 0) {
-            int v = pq.poll();
-            delete.put(v, delete.get(v) - 1);
-            if (delete.get(v) == 0)
-                delete.remove(v);
-        }
+        return (int)totalSum/(m - 2 * k);
     }
 }
+
+/**
+ * Your MKAverage object will be instantiated and called as such:
+ * MKAverage obj = new MKAverage(m, k);
+ * obj.addElement(num);
+ * int param_2 = obj.calculateMKAverage();
+ */
