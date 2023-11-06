1cbd6f29dc07febce0c5b59938710256ecc40361
Vandana <vandanakumari13998@gmail.com>
2023-11-06 16:36:21 +0530

    Time: 5 ms (99.09%) | Memory: 58.1 MB (73.82%) - LeetSync

--- Arrays/Easy/2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.java
+++ Arrays/Easy/2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.java
@@ -1,42 +1,34 @@
 class Solution {
     public int[] findOriginalArray(int[] changed) {
         int n = changed.length;
-        if(n<2) {
+        if(n<2 || n%2!=0) {
             return new int[0];
         }
-        int [] orgArray = new int[n/2];
-        Arrays.sort(changed);
-        
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int [] orgArr = new int[n/2];
+        int maxVal = Integer.MIN_VALUE;
         for(int i=0; i<n; i++) {
-            map.put(changed[i], map.getOrDefault(changed[i], 0) + 1);
+            maxVal = Math.max(maxVal, changed[i]);
         }
-        
-        for(int i=0, k=0; i<n; i++) {
-            // [1,3,4,2,6,8]
-            // 1, 2, 3, 4, 6, 8
-            
-            if(map.containsKey(changed[i])) {
-                int doubleInt = changed[i] * 2;
-                map.put(changed[i], map.get(changed[i]) - 1);
-                if(map.get(changed[i]) == 0) {
-                    map.remove(changed[i]);
-                }
-                if(map.containsKey(doubleInt)) {
-                    orgArray[k++] = changed[i];
-                    map.put(doubleInt, map.get(doubleInt) - 1);
-                    if(map.get(doubleInt) == 0) {
-                        map.remove(doubleInt);
-                    }
+
+        int [] freq = new int[maxVal+1];
+
+        for(int num: changed) {
+            freq[num]++;
+        }
+
+        for(int i=0, k=0; i<=maxVal; i++) {
+            if(freq[i] > 0) {
+                freq[i]--;
+                int twice = i * 2;
+                if(twice<=maxVal && freq[twice] > 0) {
+                    freq[twice]--;
+                    orgArr[k++] = i;
+                    i--;
                 } else {
                     return new int[0];
                 }
             }
-            
         }
-        if(orgArray.length != n/2){
-            return new int[0];
-        }
-        return orgArray;
+        return orgArr;
     }
 }
